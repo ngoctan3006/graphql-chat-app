c537148ad553211d0315215202ac1dfdf4e03227
Tan <ngoctana1k2014@gmail.com>
2023-10-03 17:33:21 +0700

    refresh token

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async refreshToken(req: Request, res: Response) {
+    const refreshToken = req.cookies['refresh_token'];
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token not found');
+    }
+    let payload: any;
+
+    try {
+      payload = this.jwtService.verify(refreshToken, {
+        secret: this.configService.get<string>('REFRESH_TOKEN_SECRET'),
+      });
+    } catch (error) {
+      throw new UnauthorizedException('Invalid or expired refresh token');
+    }
+    const userExists = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    if (!userExists) {
+      throw new BadRequestException('User no longer exists');
+    }
+
+    const expiresIn = 15000;
+    const expiration = Math.floor(Date.now() / 1000) + expiresIn;
+    const accessToken = this.jwtService.sign(
+      { ...payload, exp: expiration },
+      {
+        secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
+      },
+    );
+    res.cookie('access_token', accessToken, { httpOnly: true });
+
+    return accessToken;
+  }
+}
