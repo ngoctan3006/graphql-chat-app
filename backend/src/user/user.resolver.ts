f57c7c7e3e082fa31a89f95cc8bc6a1b6aa2c0fa
Tan <ngoctana1k2014@gmail.com>
2023-10-03 21:12:00 +0700

    store image

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,21 @@
 import { Resolver } from '@nestjs/graphql';
+import { createWriteStream } from 'fs';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import { UserService } from './user.service';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  private async storeImageAndGetUrl(file: GraphQLUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`;
+    const readStream = createReadStream();
+    readStream.pipe(createWriteStream(imagePath));
+    return imageUrl;
+  }
+}
