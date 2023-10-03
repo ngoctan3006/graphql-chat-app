e460db27313975b7f103b2292e09650da30042ca
Tan <ngoctana1k2014@gmail.com>
2023-10-03 17:56:30 +0700

    delete app controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -26,7 +25,7 @@ import { UserModule } from './user/user.module';
       }),
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
