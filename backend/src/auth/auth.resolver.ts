ca5891a1d9ac21cb626fe8d3f9a244e23de8424a
Tan <ngoctana1k2014@gmail.com>
2023-10-03 18:13:20 +0700

    auth resolver: register mutation

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,7 +1,28 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { BadRequestException } from '@nestjs/common';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto';
+import { RegisterResponse } from './types';
 
 @Resolver()
 export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ) {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'Password and confirm password are not the same.',
+      });
+    }
+    const { user } = await this.authService.register(registerDto, context.res);
+    return { user };
+  }
+
   @Query(() => String)
   async hello() {
     return 'hello';
