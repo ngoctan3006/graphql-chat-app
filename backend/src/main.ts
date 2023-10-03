a3437c39348ead3e8d1be45f7958d01cfbb9d206
Tan <ngoctana1k2014@gmail.com>
2023-10-03 16:52:25 +0700

    use grapql upload

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   });
   app.use(cookieParser());
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000000, maxFiles: 1 }));
 
   await app.listen(3000);
 }
