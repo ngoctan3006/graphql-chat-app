e0c76fc8df7d904f6700cc3599f9a5d539cc86f9
Tan <ngoctana1k2014@gmail.com>
2023-10-03 21:17:21 +0700

    user resolver: update profile

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,13 +1,30 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { Request } from 'express';
 import { createWriteStream } from 'fs';
 import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
 import { join } from 'path';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
 import { v4 as uuidv4 } from 'uuid';
 import { UserService } from './user.service';
+import { User } from './user.type';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => User)
+  async updateProfile(
+    @Args('fullname') fullname: string,
+    @Args('file', { type: () => GraphQLUpload, nullable: true })
+    file: GraphQLUpload.FileUpload,
+    @Context() context: { req: Request },
+  ) {
+    const imageUrl = file ? await this.storeImageAndGetUrl(file) : null;
+    const userId = context.req.user.sub;
+    return this.userService.updateProfile(userId, fullname, imageUrl);
+  }
 
   private async storeImageAndGetUrl(file: GraphQLUpload) {
     const { createReadStream, filename } = await file;
