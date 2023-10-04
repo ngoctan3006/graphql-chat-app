b6da10a9a5932ec8bc6b1e6fbc4d37215889fe4e
Tan <ngoctana1k2014@gmail.com>
2023-10-04 09:57:55 +0700

    fix user module missing jwt service

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService, UserResolver, PrismaService],
+  providers: [UserService, UserResolver, PrismaService, JwtService],
 })
 export class UserModule {}
