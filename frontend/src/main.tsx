a44e93840453bf8735c189201304c483b1a2f66e
Tan <ngoctana1k2014@gmail.com>
2023-10-14 16:49:56 +0700

    add apllo provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
+import { ApolloProvider } from '@apollo/client';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { client } from './apolloClient.ts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <ApolloProvider client={client}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
