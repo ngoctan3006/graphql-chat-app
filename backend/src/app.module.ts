2402e297fd532c1d5a24b646ad7de777513a2273
Tan <ngoctana1k2014@gmail.com>
2023-10-15 11:22:19 +0700

    setup ws on app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,20 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      return Math.min(times * 50, 2000);
+    },
+  },
+});
 
 @Module({
   imports: [
@@ -18,9 +29,14 @@ import { UserModule } from './user/user.module';
       inject: [ConfigService],
       driver: ApolloDriver,
       useFactory: async (configService: ConfigService) => ({
+        installSubscriptionHandlers: true,
         playground: true,
         autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         sortSchema: true,
+        subscriptions: {
+          'graphql-ws': true,
+          'subscriptions-transport-ws': true,
+        },
       }),
     }),
   ],
