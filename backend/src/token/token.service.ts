0dffd467a2e68853c2b18ef7a761b57f47c8ebb4
Tan <ngoctana1k2014@gmail.com>
2023-10-15 11:28:17 +0700

    extractToken

--- backend/src/token/token.service.ts
+++ backend/src/token/token.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+  constructor(private readonly configService: ConfigService) {}
+
+  extractToken(connectionParams: any): string | null {
+    return connectionParams?.token || null;
+  }
+}
