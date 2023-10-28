8b35c55a2a04453ff406375221de65f05559e948
Tan <ngoctana1k2014@gmail.com>
2023-10-29 01:26:11 +0700

    add sidebar styles

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,4 +1,32 @@
+import { createStyles, rem } from '@mantine/core';
 import React from 'react';
+
+const useStyles = createStyles((theme) => {
+  return {
+    link: {
+      width: rem(50),
+      height: rem(50),
+      borderRadius: theme.radius.md,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+      },
+    },
+    active: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({
+          variant: 'light',
+          color: theme.primaryColor,
+        }).background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    },
+  };
+});
 
 const Sidebar: React.FC = () => {
   return <div>Sidebar</div>;
