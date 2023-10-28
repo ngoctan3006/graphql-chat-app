e5fa08ed70352aee4e8d88856f9f912799f5a1b6
Tan <ngoctana1k2014@gmail.com>
2023-10-28 18:15:41 +0700

    add protected router

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import MainLayout from '../layouts/MainLayout';
+import ProtectedRoutes from '../components/ProtectedRoutes';
 import Sidebar from '../components/Sidebar';
+import MainLayout from '../layouts/MainLayout';
 
 const Home: React.FC = () => {
   return (
     <MainLayout>
-      <>
+      <ProtectedRoutes>
         <Sidebar />
         HOME PAGE
-      </>
+      </ProtectedRoutes>
     </MainLayout>
   );
 };
