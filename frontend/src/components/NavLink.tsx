4936a9b5bfb092cbb8e82e05eb64bc2e3c5d6759
Tan <ngoctana1k2014@gmail.com>
2023-10-29 02:22:38 +0700

    add nav link

---
+++ frontend/src/components/NavLink.tsx
@@ -0,0 +1,49 @@
+import { Tooltip, UnstyledButton, createStyles, rem } from '@mantine/core';
+import React from 'react';
+
+interface NavbarLinkProps {
+  icon: React.FC<any>;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+const useStyles = createStyles((theme) => {
+  return {
+    link: {
+      width: rem(50),
+      height: rem(50),
+      borderRadius: theme.radius.md,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+      },
+    },
+    active: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({
+          variant: 'light',
+          color: theme.primaryColor,
+        }).background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    },
+  };
+});
+
+const NavbarLink: React.FC<NavbarLinkProps> = ({ icon: Icon, label, active, onClick }) => {
+  const { classes, cx } = useStyles();
+  return (
+    <Tooltip label={label} position='top-start' offset={-30} transitionProps={{ duration: 0 }}>
+      <UnstyledButton onClick={onClick} className={cx(classes.link, { [classes.active]: active })}>
+        <Icon size='1.2rem' stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+};
+
+export default NavbarLink;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from '@apollo/client';
-import { Center, Navbar, Stack, Tooltip, UnstyledButton, createStyles, rem } from '@mantine/core';
+import { Center, Navbar, Stack, rem } from '@mantine/core';
 import {
   IconBrandMessenger,
   IconBrandWechat,
@@ -11,51 +11,7 @@ import React, { useState } from 'react';
 import { LOGOUT_USER } from '../graphql/mutations/Logout';
 import { useGeneralStore } from '../stores/generalStore';
 import { useUserStore } from '../stores/userStore';
-
-const useStyles = createStyles((theme) => {
-  return {
-    link: {
-      width: rem(50),
-      height: rem(50),
-      borderRadius: theme.radius.md,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-
-      '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-      },
-    },
-    active: {
-      '&, &:hover': {
-        backgroundColor: theme.fn.variant({
-          variant: 'light',
-          color: theme.primaryColor,
-        }).background,
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-      },
-    },
-  };
-});
-
-interface NavbarLinkProps {
-  icon: React.FC<any>;
-  label: string;
-  active?: boolean;
-  onClick?(): void;
-}
-
-const NavbarLink: React.FC<NavbarLinkProps> = ({ icon: Icon, label, active, onClick }) => {
-  const { classes, cx } = useStyles();
-  return (
-    <Tooltip label={label} position='top-start' offset={-30} transitionProps={{ duration: 0 }}>
-      <UnstyledButton onClick={onClick} className={cx(classes.link, { [classes.active]: active })}>
-        <Icon size='1.2rem' stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  );
-};
+import NavbarLink from './NavLink';
 
 const mockdata = [{ icon: IconBrandWechat, label: 'Chatrooms' }];
 
