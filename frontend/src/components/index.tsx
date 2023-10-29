5d8c6d6ddc9e72dc0ff088611010416bab53bf75
Tan <ngoctana1k2014@gmail.com>
2023-10-30 00:15:26 +0700

    add RoomList component

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -4,4 +4,5 @@ export { default as NavbarLink } from './NavLink';
 export { default as ProfileSetting } from './ProfileSetting';
 export { default as ProtectedRoutes } from './ProtectedRoutes';
 export { default as Register } from './Register';
+export { default as RoomList } from './RoomList';
 export { default as Sidebar } from './Sidebar';
