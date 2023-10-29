afe720f6d7da64ab10576164bee781ccaac6e731
Tan <ngoctana1k2014@gmail.com>
2023-10-29 22:07:03 +0700

    chatroom service: getChatroom

--- backend/src/chatroom/chatroom.service.ts
+++ backend/src/chatroom/chatroom.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ChatroomService {}
+export class ChatroomService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getChatroom(id: string) {
+    return this.prisma.chatroom.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+  }
+}
