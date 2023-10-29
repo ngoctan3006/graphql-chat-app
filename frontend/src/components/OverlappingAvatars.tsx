e3bb5f309946c6dfc9fa991cc2367c7b416e352c
Tan <ngoctana1k2014@gmail.com>
2023-10-30 00:18:53 +0700

    add OverlappingAvatars component

---
+++ frontend/src/components/OverlappingAvatars.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OverlappingAvatars: React.FC = () => {
+  return <div>OverlappingAvatars</div>;
+};
+
+export default OverlappingAvatars;
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as AuthOverlay } from './AuthOverlay';
 export { default as Login } from './Login';
 export { default as NavbarLink } from './NavLink';
+export { default as OverlappingAvatars } from './OverlappingAvatars';
 export { default as ProfileSetting } from './ProfileSetting';
 export { default as ProtectedRoutes } from './ProtectedRoutes';
 export { default as Register } from './Register';
