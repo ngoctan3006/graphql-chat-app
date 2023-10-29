62a7ce0641713415f78a0d0634ed8ee35a7ef143
Tan <ngoctana1k2014@gmail.com>
2023-10-29 10:28:58 +0700

    add register component

--- frontend/src/components/AuthOverlay.tsx
+++ frontend/src/components/AuthOverlay.tsx
@@ -1,14 +1,20 @@
 import { Modal } from '@mantine/core';
-import React from 'react';
+import React, { useState } from 'react';
+import { Register } from '.';
 import { useGeneralStore } from '../stores';
 
 const AuthOverlay: React.FC = () => {
   const isLoginModalOpen = useGeneralStore((state) => state.isLoginModalOpen);
   const toggleLoginModal = useGeneralStore((state) => state.toggleLoginModal);
 
+  const [isRegister, setIsRegister] = useState(true);
+  const toggleForm = () => {
+    setIsRegister(!isRegister);
+  };
+
   return (
     <Modal centered opened={isLoginModalOpen} onClose={toggleLoginModal}>
-      AuthOverlay
+      <Register toggleForm={toggleForm} />
     </Modal>
   );
 };
