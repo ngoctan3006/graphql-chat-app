75cb0a5d66159ae8f47cc0117e437cf0f6b70ece
Tan <ngoctana1k2014@gmail.com>
2023-10-30 00:24:13 +0700

    final OverlappingAvatars

--- frontend/src/components/OverlappingAvatars.tsx
+++ frontend/src/components/OverlappingAvatars.tsx
@@ -1,7 +1,34 @@
+import { Avatar, Tooltip } from '@mantine/core';
 import React from 'react';
+import { User } from '../gql/graphql';
 
-const OverlappingAvatars: React.FC = () => {
-  return <div>OverlappingAvatars</div>;
+interface Props {
+  users: User[];
+}
+
+const OverlappingAvatars: React.FC<Props> = ({ users }) => {
+  const remainingUsers = users.length > 3 ? users.slice(3) : [];
+  const remainingNames = remainingUsers.map((user) => user.fullname).join(', ');
+
+  return (
+    <Tooltip.Group openDelay={300} closeDelay={100}>
+      <Avatar.Group spacing='sm'>
+        <>
+          {users.slice(0, 3).map((user) => (
+            <Tooltip key={user.id} label={user.fullname}>
+              <Avatar src={user.avatarUrl || null} radius='xl' alt={user.fullname} size='lg' />
+            </Tooltip>
+          ))}
+
+          {users.length > 3 && (
+            <Tooltip label={remainingNames}>
+              <Avatar size='lg' radius='xl' children={`+${users.length - 3}`} />
+            </Tooltip>
+          )}
+        </>
+      </Avatar.Group>
+    </Tooltip.Group>
+  );
 };
 
 export default OverlappingAvatars;
